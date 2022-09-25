0e86307e103fa6723cf81d8ddd0042b90b9cb01c
Atharva Upadhye <70993547+atharva-u-01@users.noreply.github.com>
2022-09-25 16:16:31 +0530

    basic react config using pnpm babel webpack

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # custom-react-app
+
+## who calls whom
+
+- webpack-cli
+  - webpack
+  - `webpack.config.js`
+    - webpack-dev-server
+    - babel-loader
+      - @babel/core
+      - `babel.config.json`
+        - @babel/preset-react
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+};
